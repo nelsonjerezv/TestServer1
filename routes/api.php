721138229ca2e2d1ec62b6e4f8375eb8994b0feb
Nelson Jerez <nelson.jerezv@gmail.com>
2021-02-10 23:29:23 -0300

    90% de plantilla de articles

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// LIst articles
+Route::get('articles', 'ArticleController@index');
+Route::get('article/{id}', 'ArticleController@show');
+Route::post('article', 'ArticleController@store');
+Route::put('articles', 'ArticleController@store');
+Route::delete('articles', 'ArticleController@destroy');
