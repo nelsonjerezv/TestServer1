e11b84b2f7e541234f2250530f5ae51e0a9ed67c
Nelson Jerez <nelson.jerezv@gmail.com>
2021-03-08 20:23:18 -0300

    fix guardado https

--- routes/web.php
+++ routes/web.php
@@ -9,44 +9,46 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return redirect('/formularios');
-});
-
-
-Route::get('/formularios', 'OrdenTrabajoTerrenoController@index');
-Route::get('/formularios/todas-las-ordenes', 'OrdenTrabajoTerrenoController@todasLasOrdenes');
-Route::get('/formularios/editar-formulario', 'OrdenTrabajoTerrenoController@editarFormulario');
-Route::post('/formularios/guardar-formulario', 'OrdenTrabajoTerrenoController@guardarFormulario');
-Route::post('/formularios/guardar-ensayo-compresion', 'OrdenTrabajoTerrenoController@guardarFormulario');
-Route::post('/formularios/eliminar-formulario', 'OrdenTrabajoTerrenoController@eliminarFormulario');
-Route::resource('formularios', 'OrdenTrabajoTerrenoController');
+Route::group(['middleware' => ['cors']], function () {
+    Route::get('/', function () {
+        return redirect('/formularios');
+    });
 
 
-Route::get('/ensayos', 'EnsayosHormigonController@index');
-Route::get('/ensayos/todos-los-ensayos', 'EnsayosHormigonController@todosLosEnsayos');
-Route::get('/ensayos/editar-ensayo', 'EnsayosHormigonController@editarEnsayo');
-Route::post('/ensayos/guardar-ensayo-compresion', 'EnsayosHormigonController@guardarEnsayo');
-Route::post('/ensayos/eliminar-ensayo', 'EnsayosHormigonController@eliminarEnsayo');
-Route::resource('ensayos', 'EnsayosHormigonController');
+    Route::get('/formularios', 'OrdenTrabajoTerrenoController@index');
+    Route::get('/formularios/todas-las-ordenes', 'OrdenTrabajoTerrenoController@todasLasOrdenes');
+    Route::get('/formularios/editar-formulario', 'OrdenTrabajoTerrenoController@editarFormulario');
+    Route::post('/formularios/guardar-formulario', 'OrdenTrabajoTerrenoController@guardarFormulario');
+    Route::post('/formularios/guardar-ensayo-compresion', 'OrdenTrabajoTerrenoController@guardarFormulario');
+    Route::post('/formularios/eliminar-formulario', 'OrdenTrabajoTerrenoController@eliminarFormulario');
+    Route::resource('formularios', 'OrdenTrabajoTerrenoController');
+
+
+    Route::get('/ensayos', 'EnsayosHormigonController@index');
+    Route::get('/ensayos/todos-los-ensayos', 'EnsayosHormigonController@todosLosEnsayos');
+    Route::get('/ensayos/editar-ensayo', 'EnsayosHormigonController@editarEnsayo');
+    Route::post('/ensayos/guardar-ensayo-compresion', 'EnsayosHormigonController@guardarEnsayo');
+    Route::post('/ensayos/eliminar-ensayo', 'EnsayosHormigonController@eliminarEnsayo');
+    Route::resource('ensayos', 'EnsayosHormigonController');
 
 
 
-Route::get('/informeHormigon', 'EnsayosHormigonController@index');
+    Route::get('/informeHormigon', 'EnsayosHormigonController@index');
 
-Route::get('/imprimir-informe-hormigon/{indice}', function ($indice) {
-    // $pdf = PDF::loadView('informe_hormigon');
-    // return $pdf->download('informe_hormigon.pdf');
+    Route::get('/imprimir-informe-hormigon/{indice}', function ($indice) {
+        // $pdf = PDF::loadView('informe_hormigon');
+        // return $pdf->download('informe_hormigon.pdf');
 
-    $pdf = PDF::loadView('informe_hormigon', ['indice' => $indice]);
-    return $pdf->stream();
-});
+        $pdf = PDF::loadView('informe_hormigon', ['indice' => $indice]);
+        return $pdf->stream();
+    });
 
-Route::get('/imprimir-informe-hormigon', function () {
-    // $pdf = PDF::loadView('informe_hormigon');
-    // return $pdf->download('informe_hormigon.pdf');
+    Route::get('/imprimir-informe-hormigon', function () {
+        // $pdf = PDF::loadView('informe_hormigon');
+        // return $pdf->download('informe_hormigon.pdf');
 
-    $pdf = PDF::loadView('informe_hormigon');
-    return $pdf->download('informe_hormigon.pdf');
+        $pdf = PDF::loadView('informe_hormigon');
+        return $pdf->download('informe_hormigon.pdf');
+    });
+
 });
