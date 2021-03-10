a250e580a8c6da77dd4c6bf8f345e606ae72f546
Nelson Jerez <nelson.jerezv@gmail.com>
2021-03-10 18:24:36 -0300

    intento 1 pdf en heroku

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,10 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        // 'binary'  => env('WKHTML_PDF_BINARY', '"C:/xampp/htdocs/TestServer1/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64"'),
-        'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        // windows
+        // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        // linux
+        'binary'  => 'wkhtmltopdf',
         'timeout' => false,
         'options' => [],
         'env'     => [],
@@ -44,7 +46,10 @@ return [
 
     'image' => [
         'enabled' => true,
-        'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage'),
+        // windows
+        // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage'),
+        // linux
+        'binary'  => 'wkhtmltoimage',
         'timeout' => false,
         'options' => [],
         'env'     => [],
