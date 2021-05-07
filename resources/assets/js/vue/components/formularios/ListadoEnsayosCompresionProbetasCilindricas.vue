80e757342fc690fc762768ab411624f18ddae695
Nelson <nelson.jerezv@gmail.com>
2021-05-06 22:31:45 -0400

    fixed static size in item lists

--- resources/assets/js/vue/components/formularios/ListadoEnsayosCompresionProbetasCilindricas.vue
+++ resources/assets/js/vue/components/formularios/ListadoEnsayosCompresionProbetasCilindricas.vue
@@ -1,82 +1,93 @@
 <template>
-    <div class="contenedor-vue">
-        <div class="container-elementos">
-            <h1>Listado Ensayos Compresi&oacute;n</h1>
-            <el-input class="width-100" placeholder="Filtre por N° Muestra" v-model="buscador"></el-input>
-        </div>
-        <div class="container-items-formulario">
-            <item-lista-ensayo
-                v-for="(orden, index) in ensayosHormigon"
-                v-bind:itemLista="orden"
-                v-bind:index="index"
-                v-bind:key="orden.id"
-                @actualizar="getEnsayos"
-            />
-        </div>
+  <div class="contenedor-vue" style="display: flex; flex-direction: column">
+    <div class="container-elementos">
+      <h1>Listado Ensayos Compresi&oacute;n</h1>
+      <el-input
+        class="width-100"
+        placeholder="Filtre por N° Muestra"
+        v-model="buscador"
+      ></el-input>
     </div>
+    <div class="container-items-formulario">
+      <item-lista-ensayo
+        v-for="(orden, index) in ensayosHormigon"
+        v-bind:itemLista="orden"
+        v-bind:index="index"
+        v-bind:key="orden.id"
+        @actualizar="getEnsayos"
+      />
+    </div>
+  </div>
 </template>
 <script>
-    import ItemListaEnsayo from '../../components/formularios/ItemListaEnsayo.vue';
+import ItemListaEnsayo from "../../components/formularios/ItemListaEnsayo.vue";
 
-    export default {
-        components: {
-            ItemListaEnsayo
+export default {
+  components: {
+    ItemListaEnsayo,
+  },
+  props: ["modo"],
+  data() {
+    return {
+      urlTodosLosEnsayos: `${GLOBAL.URL}ensayos/todos-los-ensayos`,
+      ensayosHormigon: "",
+      todosLosEnsayos: "",
+      buscador: "",
+    };
+  },
+  mounted() {
+    this.getEnsayos();
+  },
+  methods: {
+    getEnsayos() {
+      this.ensayosHormigon = [];
+      this.todosLosEnsayos = [];
+      this.$http.get(`${this.urlTodosLosEnsayos}/${this.modo}`).then(
+        (response) => {
+          this.todosLosEnsayos = response.body;
+          this.ensayosHormigon = response.body;
         },
-        props: [
-            'modo'
-        ],
-        data(){
-            return{
-                urlTodosLosEnsayos: `${GLOBAL.URL}ensayos/todos-los-ensayos`,
-                ensayosHormigon: '',
-                todosLosEnsayos: '',
-                buscador: ''
-            }
-        },
-        mounted(){
-            this.getEnsayos();
-        },
-        methods: {
-            getEnsayos() {
-                this.ensayosHormigon = [];
-                this.todosLosEnsayos = [];
-                this.$http.get(`${this.urlTodosLosEnsayos}/${this.modo}`)
-                .then(response => {
-                    this.todosLosEnsayos = response.body;
-                    this.ensayosHormigon = response.body;
-                }, response => {
-                    Tools.mensajeAlerta("No se pueden cargar los ensayos.", Tools.MENSAJE.ERROR, '', 5);
-                });
-            },
-            filtraEnsayos(){
-                this.ensayosHormigon = this.todosLosEnsayos.filter(ensayo =>  ensayo.ott.toString().includes(this.buscador) ||
-                                                                              ensayo.num_ingreso.toString().includes(this.buscador) ||
-                                                                              ensayo.num_informe.includes(this.buscador)
-                                                            );
-            }
-        },
-        watch: {
-            buscador() {
-                this.filtraEnsayos();
-            },
-            modo(){
-                this.getEnsayos();
-            }
-        },
-    }
+        (response) => {
+          Tools.mensajeAlerta(
+            "No se pueden cargar los ensayos.",
+            Tools.MENSAJE.ERROR,
+            "",
+            5
+          );
+        }
+      );
+    },
+    filtraEnsayos() {
+      this.ensayosHormigon = this.todosLosEnsayos.filter(
+        (ensayo) =>
+          ensayo.ott.toString().includes(this.buscador) ||
+          ensayo.num_ingreso.toString().includes(this.buscador) ||
+          ensayo.num_informe.includes(this.buscador)
+      );
+    },
+  },
+  watch: {
+    buscador() {
+      this.filtraEnsayos();
+    },
+    modo() {
+      this.getEnsayos();
+    },
+  },
+};
 </script>
 <style>
-    .width-100{
-        width: 100%;
-    }
-    .container-elementos{
-        margin-bottom: 20px;
-    }
-    .container-items-formulario{
-        max-height: calc(100% - 128px);
-        overflow: auto;
-        padding: 10px;
-    }
+.width-100 {
+  width: 100%;
+}
+.container-elementos {
+  margin-bottom: 20px;
+}
+.container-items-formulario {
+  max-height: calc(100% - 128px);
+  overflow: auto;
+  padding: 10px;
+}
 </style>
 
 
--- resources/assets/js/vue/components/formularios/ListadoFormularios.vue
+++ resources/assets/js/vue/components/formularios/ListadoFormularios.vue
@@ -1,82 +1,95 @@
 <template>
-    <div class="contenedor-vue">
-        <div class="container-elementos">
-            <h1>Listado Órdenes de Trabajo de Terreno</h1>
-            <el-input class="width-100" placeholder="Filtre por N° OTT, N° Cliente Obra o Laboratorista" v-model="buscador"></el-input>
-        </div>
-        <div class="container-items-formulario">
-            <item-lista-formularios
-                v-for="(orden, index) in ordenesDeTrabajo"
-                v-bind:itemLista="orden"
-                v-bind:index="index"
-                v-bind:key="orden.id"
-                @actualizar="getOrdenes"
-            />
-        </div>
+  <div class="contenedor-vue" style="display: flex; flex-direction: column">
+    <div class="container-elementos">
+      <h1>Listado Órdenes de Trabajo de Terreno</h1>
+      <el-input
+        class="width-100"
+        placeholder="Filtre por N° OTT, N° Cliente Obra o Laboratorista"
+        v-model="buscador"
+      ></el-input>
     </div>
+    <div class="container-items-formulario">
+      <item-lista-formularios
+        v-for="(orden, index) in ordenesDeTrabajo"
+        v-bind:itemLista="orden"
+        v-bind:index="index"
+        v-bind:key="orden.id"
+        @actualizar="getOrdenes"
+      />
+    </div>
+  </div>
 </template>
 <script>
-    import ItemListaFormularios from '../../components/formularios/ItemListaFormularios.vue';
+import ItemListaFormularios from "../../components/formularios/ItemListaFormularios.vue";
 
-    export default {
-        components: {
-            ItemListaFormularios
+export default {
+  components: {
+    ItemListaFormularios,
+  },
+  props: ["modo"],
+  data() {
+    return {
+      urltodasLasOrdenes: `${GLOBAL.URL}formularios/todas-las-ordenes`,
+      ordenesDeTrabajo: "",
+      todasLasOrdenes: "",
+      buscador: "",
+    };
+  },
+  mounted() {
+    this.getOrdenes();
+  },
+  methods: {
+    getOrdenes() {
+      this.ordenesDeTrabajo = [];
+      this.todasLasOrdenes = [];
+      this.$http.get(`${this.urltodasLasOrdenes}/${this.modo}`).then(
+        (response) => {
+          this.todasLasOrdenes = response.body;
+          this.ordenesDeTrabajo = response.body;
         },
-        props: [
-            'modo'
-        ],
-        data(){
-            return{
-                urltodasLasOrdenes: `${GLOBAL.URL}formularios/todas-las-ordenes`,
-                ordenesDeTrabajo: '',
-                todasLasOrdenes: '',
-                buscador: ''
-            }
-        },
-        mounted(){
-            this.getOrdenes();
-        },
-        methods: {
-            getOrdenes() {
-                this.ordenesDeTrabajo = [];
-                this.todasLasOrdenes =  [];
-                this.$http.get(`${this.urltodasLasOrdenes}/${this.modo}`)
-                .then(response => {
-                    this.todasLasOrdenes =  response.body;
-                    this.ordenesDeTrabajo = response.body;
-                }, response => {
-                    Tools.mensajeAlerta("No se pueden cargar las ordenes.", Tools.MENSAJE.ERROR, '', 5);
-                });
-            },
-            filtraOrdenes(){
-                this.ordenesDeTrabajo = this.todasLasOrdenes.filter(orden => orden.num_cliente_obra.includes(this.buscador) ||
-                                                                             orden.laboratorista.toLowerCase().includes(this.buscador.toLowerCase()) ||
-                                                                             orden.id.toString().includes(this.buscador)
-                                                                    );
-            }
-        },
-        watch: {
-            buscador() {
-                this.filtraOrdenes();
-            },
-            modo(){
-                this.getOrdenes();
-            }
-        },
-    }
+        (response) => {
+          Tools.mensajeAlerta(
+            "No se pueden cargar las ordenes.",
+            Tools.MENSAJE.ERROR,
+            "",
+            5
+          );
+        }
+      );
+    },
+    filtraOrdenes() {
+      this.ordenesDeTrabajo = this.todasLasOrdenes.filter(
+        (orden) =>
+          orden.num_cliente_obra.includes(this.buscador) ||
+          orden.laboratorista
+            .toLowerCase()
+            .includes(this.buscador.toLowerCase()) ||
+          orden.id.toString().includes(this.buscador)
+      );
+    },
+  },
+  watch: {
+    buscador() {
+      this.filtraOrdenes();
+    },
+    modo() {
+      this.getOrdenes();
+    },
+  },
+};
 </script>
 <style>
-    .width-100{
-        width: 100%;
-    }
-    .container-elementos{
-        margin-bottom: 20px;
-    }
-    .container-items-formulario{
-        max-height: calc(100% - 128px);
-        overflow: auto;
-        padding: 10px;
-    }
+.width-100 {
+  width: 100%;
+}
+.container-elementos {
+  margin-bottom: 20px;
+}
+.container-items-formulario {
+  max-height: calc(100% - 128px);
+  overflow: auto;
+  padding: 10px;
+}
 </style>
 
 
